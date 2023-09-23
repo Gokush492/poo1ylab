e5116f85c727c644dcbc42defa064683c473d6db
Gokush492 <xmakaveli02@gmail.com>
2023-09-22 18:50:10 -0600

    Actividad 11

---
+++ _p68_TablaMultiplicar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class _p68_TablaMultiplicar {
+
+    public static void Tabla(int t, int n) {
+        System.out.printf("\nTabla del %d\n",t);
+        for(int i=1; i <= n; i++){
+            System.out.printf("%d x %d = %d\n",t,i,t*i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int t,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\033[H\033[2J");System.out.flush();
+        System.out.print("Que tabla quieres ? "); t = sc.nextInt();
+        System.out.print("Hata donde la quieres ? "); n = sc.nextInt();
+        Tabla(t,n);
+    }
+}
