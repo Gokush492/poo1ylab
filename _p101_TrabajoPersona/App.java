206a16fa3c9fceb88c78dd3d8ba93c13299b6e5d
Gokush492 <xmakaveli02@gmail.com>
2023-10-13 19:28:42 -0600

    Actividad 16

---
+++ _p101_TrabajoPersona/App.java
@@ -0,0 +1,20 @@
+package _p101_TrabajoPersona;
+
+public class App {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        Trabajo trabajo = new Trabajo();
+        
+        trabajo.setId(1);
+        trabajo.setRol("Chofer");
+        trabajo.setSalario(200);
+        persona1.setNombre("Julia Urbina");
+        persona1.setTrabajo(trabajo);
+
+        System.out.print("\033[H\033[2J");System.out.flush();
+        System.out.println(persona1);
+
+        Persona persona2 = new Persona("Julion Alvarez",new Trabajo(0, "Cantante", 10));
+        System.out.println(persona2);
+    }
+}
